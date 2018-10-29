e8cd303cad4f4a4d1518220eaec962ce660e3c56
xdghcnt <xd-mail@ya.ru>
2018-10-29 16:20:14 +0300

    Fixing random

--- module.js
+++ module.js
@@ -467,7 +467,14 @@ function init(wsServer, path) {
     }
 
     function shuffleArray(array) {
-        array.sort(() => (Math.random() - 0.5));
+        let currentIndex = array.length, temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
         return array;
     }
 
