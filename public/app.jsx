727be2221c1228efdcc4d44e96e0e0cd7ced394d
xdghcnt <xd-mail@ya.ru>
2019-12-25 20:18:18 +0300

    CF support fixed

--- public/app.jsx
+++ public/app.jsx
@@ -227,7 +227,9 @@ class Game extends React.Component {
             localStorage.userToken = makeId();
         }
         if (!location.hash)
-            history.replaceState(undefined, undefined, "#" + makeId());
+            history.replaceState(undefined, undefined, location.origin + location.pathname + "#" + makeId());
+        else
+            history.replaceState(undefined, undefined, location.origin + location.pathname + location.hash);
         if (localStorage.acceptDelete) {
             initArgs.acceptDelete = localStorage.acceptDelete;
             delete localStorage.acceptDelete;
