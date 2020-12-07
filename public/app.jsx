e4a89c35a579aa1e3fbb8db492c7b74321e5c946
xdghcnt <xd-mail@ya.ru>
2020-12-07 03:22:16 +0300

    Mobile edit points support

--- public/app.jsx
+++ public/app.jsx
@@ -101,7 +101,11 @@ class Words extends React.Component {
                     reported: data.activeWordReported
                 }])).map((word, index) => (
                     <div className={cs("word", {reported: word.reported})}
-                         onTouchStart={(e) => e.target.focus()}>&nbsp;{hyphenate(word.word)}&nbsp;
+                         onTouchStart={(e) => e.target.focus()}
+                    >
+                        {game.isMobile ? <div className="points-button remove-points" onClick={() =>
+                            game.handleRemoveWordPoints(index)}>−</div> : ""}
+                        &nbsp;{hyphenate(word.word)}&nbsp;
                         <input
                             className={cs({positive: word.points > 0, negative: word.points < 0})}
                             type="number" value={word.points} min="-2" max="1"
@@ -141,6 +145,8 @@ class Words extends React.Component {
                                     report_problem
                                 </i>
                             </div>) : ""}
+                        {game.isMobile ? <div className="points-button add-points" onClick={() =>
+                            game.handleAddWordPoints(index)}>+</div> : ""}
                     </div>
                 )))}
             </div>
@@ -414,6 +420,11 @@ class Game extends React.Component {
             if (evt.code === "Space" && !keyDownSubscriber)
                 subscribeKeydown();
         });
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+            || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+            document.body.classList.add("is-mobile");
+            this.isMobile = true;
+        }
     }
 
     updateState() {
@@ -487,6 +498,22 @@ class Game extends React.Component {
 
     handleChangeWordPoints(id, value) {
         if (value > -2 && value < 2) {
+            this.state.currentWords[id].points = value;
+            this.socket.emit("set-word-points", this.state.currentWords);
+        }
+    }
+
+    handleAddWordPoints(id) {
+        const value = this.state.currentWords[id].points + 1;
+        if (value < 2) {
+            this.state.currentWords[id].points = value;
+            this.socket.emit("set-word-points", this.state.currentWords);
+        }
+    }
+
+    handleRemoveWordPoints(id) {
+        const value = this.state.currentWords[id].points - 1;
+        if (value > -2) {
             this.state.currentWords[id].points = value;
             this.socket.emit("set-word-points", this.state.currentWords);
         }
@@ -910,7 +937,8 @@ class Game extends React.Component {
                     <div className={cs("game-board", {
                         active: this.state.inited,
                         "game-over": gameIsOver,
-                        "solo-mode": this.state.soloMode
+                        "solo-mode": this.state.soloMode,
+                        isMobile: this.isMobile
                     })}>
                         <div className="teams-pane">
                             <div
